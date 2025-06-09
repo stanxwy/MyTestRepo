9f0588af74f5373f43bd6a01762ea1dcef50da2c
stx <xiongwenyan@gmail.com>
2025-06-09 22:07:30 +0800

    log added

--- demo/src/main/java/com/example/demo/controller/OrderController.java
+++ demo/src/main/java/com/example/demo/controller/OrderController.java
@@ -2,12 +2,16 @@ package com.example.demo.controller;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+
+    Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Value("${server.port}")
     private String port;
@@ -19,6 +23,7 @@ public class OrderController {
 
     @RequestMapping("/order/index")
     public String index() {
+        logger.info("accessing /order/index");
         return "index " + port + " " + UUID.randomUUID().toString();
     }
 }
